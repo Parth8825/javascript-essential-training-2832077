16f011c0df79346efa48173e4488fbeb9706a1d3
Parth Darji <pdarji8825@conestogac.on.ca>
2022-02-28 17:14:20 -0500

    practice

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,8 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+import Drawer from "./drawer.js";
+
+const everyDrawer = new Drawer("My things", 90, "Red", 3, 10, 15, false);
+
+console.log("The everDrawer object:", everyDrawer);
