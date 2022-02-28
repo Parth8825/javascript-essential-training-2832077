0337b7e77797ed042a2d31d14df9073e391e7641
Parth Darji <pdarji8825@conestogac.on.ca>
2022-02-28 16:32:14 -0500

    Practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,24 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const drawer = {
+  name: "My personal things",
+  volume: 90,
+  color: "blue",
+  drawerBox: 3,
+  boxsize: {
+    hieght: 10,
+    width: 15,
+  },
+  boxOpen: false,
+  toggleLid: function (boxStatus) {
+    this.boxOpen = boxStatus;
+  },
+  newBoxSize: function (sizeHieght, sizeWidth) {
+    this.boxsize.hieght = sizeHieght;
+    this.boxsize.width = sizeWidth;
+  },
+};
+
+console.log("Ther Drawr object = ", drawer);
